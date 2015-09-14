1d0cc4a72ff20b7fc07ce08d06aa1c00222f3d7a
s-p-n <coderspence@gmail.com>
2015-09-14 05:33:38 -0600

    added socket.io and basic client code

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')('http');
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/client', function req, res) {
+	res.sendFile(__dirname + '/client.js');
+}
+
+io.on('connection', function (socket) {
+	console.log('a user connected');
+})
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
