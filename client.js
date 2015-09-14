1e5def1cbebb79325af00860212f6c16fbe20782
s-p-n <coderspence@gmail.com>
2015-09-14 06:55:54 -0600

    Working Chat with Nickname

--- client.js
+++ client.js
@@ -1,1 +1,20 @@
 var socket = io();
+
+var i;
+for (i = 0; i < message_history.length; i += 1) {
+	$('#messages').append($('<li>').text(message_history[i]));
+}
+
+$('#name').blur(function () {
+	socket.emit('nickname', $('#name').val());
+})
+
+$('form').submit(function(){
+	socket.emit('message', $('#m').val());
+	$('#m').val('');
+	return false;
+});
+
+socket.on('chat message', function (msg) {
+	$('#messages').append($('<li>').text(msg));
+});
--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')('http');
+var io = require('socket.io')(http);
 
+var message_log = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/client', function req, res) {
+app.get('/client', function (req, res) {
 	res.sendFile(__dirname + '/client.js');
-}
+});
+
+app.get('/history', function (req, res) {
+	res.send('var message_history = ' + JSON.stringify(message_log));
+});
 
 io.on('connection', function (socket) {
-	console.log('a user connected');
-})
+	var nickname = "Anonymous";
+	console.log('User connected.');
+	socket.on('nickname', function (name) {
+		console.log("User set nickname:", name);
+		nickname = name;
+	});
+	socket.on('message', function (msg) {
+		var message = nickname + ': ' + msg;
+		message_log.push(message);
+		console.log(message);
+		io.emit('chat message', message);
+	});
+	socket.on('disconnect', function () {
+		console.log("User disconnected.");
+	});
+});
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('Listening.');
 });
